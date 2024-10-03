d668eb783197bfc9619f718514b8a04dd9a74609
alex4013 <as8181314@gmail.com>
2024-10-02 21:14:59 -0300

    Add files via upload

---
+++ aula_02.py
@@ -0,0 +1,19 @@
+numero1=25
+numero2=10
+nome="alexsander"
+sobrenome="silva"
+nota1=80
+nota2=96
+nota3=70
+nota4=50
+largura=25
+altura=30
+
+print('a soma dos numeros eh:',numero1+numero2)
+
+print('o nome eh sobrenome:',nome,sobrenome)
+
+resultado_final =(nota1+nota2+nota3+nota4)/4
+print('a media final eh:',resultado_final)
+resultado=(largura*altura)
+print('a area do retangulo eh:',resultado, 'm2')
